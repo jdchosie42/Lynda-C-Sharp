db1420c6f88617b4fb4a24dd6406c93b35b899ec
Jeremy Chosie <jdchosie42@students.tntech.edu>
2018-05-31 16:34:53 -0400

    Created a new class and added a few fields to the class.

---
+++ SchoolApp/SchoolLibrary/School.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolLibrary
+{
+    class School
+    {
+        string name;
+        string address;
+        string city;
+        string state;
+        string zip;
+        string phoneNumber;
+    }
+}
