0733514c0deefdbf41c7e8634a78e721391b5669
Jeremy Chosie <jdchosie42@students.tntech.edu>
2018-06-03 12:53:35 -0400

    Added an interface and extension method.

--- SchoolApp/SchoolLibrary/ScienceExperiement.cs
+++ SchoolApp/SchoolLibrary/ScienceExperiement.cs
@@ -6,11 +6,13 @@ using System.Threading.Tasks;
 
 namespace SchoolLibrary
 {
-    public class ScienceExperiement
+    public class ScienceExperiement : IScored
     {
         public string Hypothesis { get; set; }
         public string Materials { get; set; }
         public string Method { get; set; }
         public string Conclusion { get; set; }
+        public float Score { get; set; }
+        public float MaximumScore { get; set; }
     }
 }
